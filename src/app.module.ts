08c722b56ae6d2aa15f50e68a292e47f63bfa3b7
aymeric <totofrancois12@gmail.com>
2026-04-23 11:40:12 +0200

    feat: configure TypeOrmModule in AppModule with all feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/user.entity';
+import { Entrepot } from './entrepots/entrepot.entity';
+import { Course } from './courses/course.entity';
+import { Incident } from './incidents/incident.entity';
+import { UsersModule } from './users/users.module';
+import { EntrepotsModule } from './entrepots/entrepots.module';
+import { CoursesModule } from './courses/courses.module';
+import { IncidentsModule } from './incidents/incidents.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [User, Entrepot, Course, Incident],
+      synchronize: true,
+    }),
+    UsersModule,
+    EntrepotsModule,
+    CoursesModule,
+    IncidentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
