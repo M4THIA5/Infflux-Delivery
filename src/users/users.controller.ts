399d1557c5a686eb2135bd0d3c7ba43b6e8d50c8
aymeric <totofrancois12@gmail.com>
2026-04-23 16:35:29 +0200

    fix: remove JwtAuthGuard from UsersController and IncidentsController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,15 +7,12 @@ import {
   Body,
   Param,
   Query,
-  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserRole } from './user.entity';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
-@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
